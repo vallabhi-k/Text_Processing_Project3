a00114d8c00f70cca09f8b4cf5d4dece6b11aaec
Vallabhi Kamalia <vallabhikamalia@Vallabhis-MacBook-Air.local>
2023-05-06 22:24:49 -0700

    first commit

--- project3/Bitap.java
+++ project3/Bitap.java
@@ -1,10 +1,47 @@
 package project3;
 
 public class Bitap {
+  public int match(String text, String pattern) {
   
-  public int match(String T, String P) {
-    /** Your code goes here */
-    return -1;
-  }
+    int m = pattern.length();
+        int n = text.length();
 
+        if (m > 31) {
+            throw new IllegalArgumentException("Pattern too long");
+        }
+
+        int[] R = new int[256];
+
+        int pattern_mask = 1 << (m - 1);
+        int i;
+
+        for (i = 0; i < 256; i++) {
+            R[i] = ~0;
+        }
+
+        for (i = 0; i < m; i++) {
+            R[pattern.charAt(i)] &= ~(1 << i);
+        }
+
+        int state = ~0;
+
+        for (i = 0; i < n - m + 1; i++) {
+            state = (state << 1) | 1;
+            state &= R[text.charAt(i)];
+
+            if ((state & pattern_mask) != 0) {
+                int j;
+                for (j = 0; j < m; j++) {
+                    if (pattern.charAt(j) != text.charAt(i + j)) {
+                        break;
+                    }
+                }
+                if (j == m) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+  }
 }
