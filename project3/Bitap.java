241413bc7bb3758502d2b017c82f57338e32033c
Vallabhi Kamalia <vallabhikamalia@Vallabhis-MacBook-Air.local>
2023-05-06 22:36:43 -0700

    first commit

--- project3/Bitap.java
+++ project3/Bitap.java
@@ -2,39 +2,41 @@ package project3;
 import java.io.*;
 
 public class Bitap {
-   
-    public int match(String t, String p) {
-        char[] text = t.toCharArray();
-        char[] pattern = p.toCharArray();
-        int index = bitap_search(text, pattern);
-        if (index == -1) 
-           return -1;
-            return index;
-        
-    }
-    private int bitap_search(char[] text, char[] pattern) {
-        int len = pattern.length;
-        long pattern_mask[] = new long[Character.MAX_VALUE + 1];
-        long R = ~1;
-        if (len == 0) {
-            return -1;
-        }
-        if (len > 63) {
-            return -1;
-        }
-        for (int i = 0; i <= Character.MAX_VALUE; ++i) {
-            pattern_mask[i] = ~0;
-        }
-        for (int i = 0; i < len; ++i) {
-            pattern_mask[pattern[i]] &= ~(1L << i);
-        }
-        for (int i = 0; i < text.length; ++i) {
-            R |= pattern_mask[text[i]];
-            R <<= 1;
-            if ((R & (1L << len)) == 0) {
-                return i - len + 1;
-            }
-        }
+  public int match(String text, String pattern) {
+    char[] textChars = text.toCharArray();
+    char[] patternChars = pattern.toCharArray();
+    int index = search(textChars, patternChars);
+    if (index == -1) 
+       return -1;
+        return index;
+    
+  }
+  public int search(char[] text, char[] pattern) {
+    int len = pattern.length;
+    long patternMask[] = new long[Character.MAX_VALUE + 1];
+    long R = ~1;
+    if (len == 0) {
         return -1;
     }
+    if (len > 63) {
+        return -1;
+    }
+    for (int i = 0; i <= Character.MAX_VALUE; ++i) {
+        patternMask[i] = ~0;
+    }
+    for (int i = 0; i < len; ++i) {
+        patternMask[pattern[i]] &= ~(1L << i);
+    }
+    for (int i = 0; i < text.length; ++i) {
+        R |= patternMask[text[i]];
+        R <<= 1;
+        if ((R & (1L << len)) == 0) {
+            return i - len + 1;
+        }
+    }
+    return -1;
+  }
 }
+
+
+
